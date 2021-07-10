56e3ad66d4990100de3d1cc9dc67d6ca688cc897
Arafat447 <tahmidhasan184@gmail.com>
2021-07-10 14:57:01 +0600

    service api added

--- index.js
+++ index.js
@@ -12,26 +12,45 @@ app.use(bodyPerser.json());
 const uri = "mongodb+srv://MyDbUser:gorurhat@cluster0.bssu4.mongodb.net/cowdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("cowdb").collection("cows");
+  const cowCollection = client.db("cowdb").collection("cows");
+  const serviceCollection = client.db("cowdb").collection("services");
   // Post
   app.post("/addCow",(req,res)=>{
       const cow = req.body;
-      collection.insertOne(cow)
+      cowCollection.insertOne(cow)
+      .then(result=>{
+        res.send("success")
+      })
+  });
+  app.post("/addService",(req,res)=>{
+      const service = req.body;
+      serviceCollection.insertOne(service)
       .then(result=>{
         console.log(result)
         res.send("success")
       })
+      console.log(service)
   });
   // Get
   app.get("/allCows",(req,res)=>{
-      collection.find({})
+    cowCollection.find({})
+      .toArray((err,data)=>{
+          res.send(data)
+      })    
+  });
+  app.get("/allServices",(req,res)=>{
+    serviceCollection.find({})
       .toArray((err,data)=>{
           res.send(data)
       })    
   });
   //Delete
-  app.delete("/delete/:id",(req,res)=>{
-      collection.deleteOne({_id:ObjectId(req.params.id)})
+  app.delete("/deleteCow/:id",(req,res)=>{
+    cowCollection.deleteOne({_id:ObjectId(req.params.id)})
+      .then(result=>res.send("Succesfully deleted"))
+  })
+  app.delete("/deleteService/:id",(req,res)=>{
+    serviceCollection.deleteOne({_id:ObjectId(req.params.id)})
       .then(result=>res.send("Succesfully deleted"))
   })
 });
